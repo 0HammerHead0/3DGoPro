e762d8352154c9bfdc5e8777eaa42f5693fca41a
0HammerHead0 <shashwatakahammer@gmail.com>
2023-12-16 15:22:33 +0530

    Working Scroll&3D-Render

--- src/script/app.js
+++ src/script/app.js
@@ -1,44 +1,58 @@
-const pages = document.querySelectorAll('.page');
+import * as THREE from 'three';
+import gsap from 'gsap';
+import '../style/style.css';
 
-function scrollToPage(index, duration) {
-  const targetPosition = pages[index].offsetTop;
-  const startPosition = window.pageYOffset || document.documentElement.scrollTop;
-  const distance = targetPosition - startPosition;
-  const startTime = performance.now();
+const container = document.querySelector('.webgl-content');
+const sizes = {
+  width: container.clientWidth * 1,
+  height: container.clientHeight * 1
+};
 
-  function scrollAnimation(currentTime) {
-    const elapsedTime = currentTime - startTime;
-    const scrollProgress = Math.min(elapsedTime / duration, 1);
-    const newPosition = startPosition + distance * scrollProgress;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({ canvas , alpha: true });
+renderer.setSize(sizes.width, sizes.height);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-    window.scrollTo(0, newPosition);
+camera.position.z = 2;
 
-    if (scrollProgress < 1) {
-      requestAnimationFrame(scrollAnimation);
-    }
+document.addEventListener('DOMContentLoaded', function() {
+  const sections = document.querySelectorAll('.scroll-area');
+
+  if (sections.length > 0) {
+    document.querySelector('.scroll-container').addEventListener('scroll', function() {
+      const scrollPos = this.scrollTop;
+      let activeSection = 0;
+      sections.forEach((section, index) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollPos >= sectionTop && scrollPos < sectionTop + sectionHeight) {
+          activeSection = index + 1;
+        }
+      });
+
+      handleSectionChange(activeSection);
+    });
   }
-
-  requestAnimationFrame(scrollAnimation);
-}
-
-function findCurrentPage() {
-  const scrollPosition = window.scrollY;
-  let currentPageIndex = 0;
-
-  pages.forEach((page, index) => {
-    const offset = page.offsetTop;
-    const pageHeight = page.offsetHeight;
-
-    if (scrollPosition >= offset && scrollPosition < offset + pageHeight) {
-      currentPageIndex = index;
-    }
-  });
-
-  return currentPageIndex;
-}
-
-document.addEventListener('scroll', () => {
-  console.log('scroll');
-  const currentPageIndex = findCurrentPage();
-  scrollToPage(currentPageIndex,1000);
 });
+
+function handleSectionChange(activeSection) {
+  // Replace this with your desired actions based on the active section
+  console.log(`Active Section: ${activeSection}`);
+  // Call your functions or trigger events here
+  // Example: YourFunctionName();
+}
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
