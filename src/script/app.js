71b0a6614a6102463161f16b0907a29f2624b115
0HammerHead0 <shashwatakahammer@gmail.com>
2023-12-16 10:20:41 +0530

    sad commit

--- src/script/app.js
+++ src/script/app.js
@@ -1,19 +1,44 @@
 const pages = document.querySelectorAll('.page');
-console.log(pages);
-document.addEventListener('scroll', () => {
-    console.log('scroll')
+
+function scrollToPage(index, duration) {
+  const targetPosition = pages[index].offsetTop;
+  const startPosition = window.pageYOffset || document.documentElement.scrollTop;
+  const distance = targetPosition - startPosition;
+  const startTime = performance.now();
+
+  function scrollAnimation(currentTime) {
+    const elapsedTime = currentTime - startTime;
+    const scrollProgress = Math.min(elapsedTime / duration, 1);
+    const newPosition = startPosition + distance * scrollProgress;
+
+    window.scrollTo(0, newPosition);
+
+    if (scrollProgress < 1) {
+      requestAnimationFrame(scrollAnimation);
+    }
+  }
+
+  requestAnimationFrame(scrollAnimation);
+}
+
+function findCurrentPage() {
   const scrollPosition = window.scrollY;
+  let currentPageIndex = 0;
+
   pages.forEach((page, index) => {
     const offset = page.offsetTop;
-    if (scrollPosition >= offset && scrollPosition < offset + window.innerHeight) {
-      scrollToPage(index);
+    const pageHeight = page.offsetHeight;
+
+    if (scrollPosition >= offset && scrollPosition < offset + pageHeight) {
+      currentPageIndex = index;
     }
   });
-});
 
-function scrollToPage(index) {
-  window.scrollTo({
-    top: pages[index].offsetTop,
-    behavior: 'smooth'
-  });
+  return currentPageIndex;
 }
+
+document.addEventListener('scroll', () => {
+  console.log('scroll');
+  const currentPageIndex = findCurrentPage();
+  scrollToPage(currentPageIndex,1000);
+});
